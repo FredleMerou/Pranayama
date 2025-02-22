db3d97892dba1934868d26f43d9c594a5853d42e
FredleMerou <fred.simion@gmail.com>
2025-02-22 18:02:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ function startPranayama() {
     const hold1 = parseInt(document.getElementById('hold1').value);
     const exhale = parseInt(document.getElementById('exhale').value);
     const hold2 = parseInt(document.getElementById('hold2').value);
+    const cycles = parseInt(document.getElementById('cycles').value);
 
     const timerDiv = document.getElementById('timer');
-    timerDiv.innerHTML = '';
+    const progressionDiv = document.getElementById('progression');
+    const cycleCounterDiv = document.getElementById('cycle-counter');
+    let currentCycle = 0;
 
     const phases = [
-        { phase: 'Inhale', time: inhale },
-        { phase: 'Hold after inhale', time: hold1 },
-        { phase: 'Exhale', time: exhale },
-        { phase: 'Hold after exhale', time: hold2 }
+        { phase: 'Inspire', time: inhale },
+        { phase: 'Rétention après inspire', time: hold1 },
+        { phase: 'Expire', time: exhale },
+        { phase: 'Rétention après expire', time: hold2 }
     ];
 
     let currentPhase = 0;
@@ -19,13 +22,23 @@ function startPranayama() {
     function nextPhase() {
         if (currentPhase < phases.length) {
             const { phase, time } = phases[currentPhase];
-            timerDiv.innerHTML = `${phase} for ${time} seconds`;
+            progressionDiv.innerHTML = `Phase : ${phase}`;
+            timerDiv.innerHTML = `${time} secondes restantes`;
             setTimeout(() => {
                 currentPhase++;
                 nextPhase();
             }, time * 1000);
         } else {
-            timerDiv.innerHTML = 'Pranayama cycle complete!';
+            currentCycle++;
+            cycleCounterDiv.innerHTML = `Cycle ${currentCycle} sur ${cycles}`;
+            if (currentCycle < cycles) {
+                currentPhase = 0;
+                nextPhase();
+            } else {
+                timerDiv.innerHTML = 'Cycle de pranayama terminé !';
+                progressionDiv.innerHTML = '';
+                cycleCounterDiv.innerHTML = '';
+            }
         }
     }
 
